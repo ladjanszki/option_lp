d506c3bb90a67ff9bd8873236e924c74c4d70146
István Ladjánszki <istvan.ladjanszki@gmail.com>
2020-03-31 19:16:55 +0200

    * entry point file renamed

--- option_lp.py
+++ option_lp.py
@@ -8,48 +8,39 @@ import util
 np.random.seed(0)
 
 
-#### HF1
+# HF1
 # arg: filename
 # arg: startPrices (list)
 # arg: branches on levels (tuple)
 # arg: mean, sigma (random generator parameters)
 lpFileName = 'inputs/hf1.lp'
-#lpFileName = 'debug.lp'
+# lpFileName = 'debug.lp'
 
-#tree = Tree()
-#tree.create_node("0", "0", data=util.StateData([1, 100, 100, 100], 0))  # root node
+# tree = Tree()
+# tree.create_node("0", "0", data=util.StateData([1, 100, 100, 100], 0))  # root node
 #
-#for idx in range(1,101):
-#    tree.create_node('0/' + str(idx), '0/' + str(idx), parent="0", data=util.stateDataIterator(tree['0'], 2, 1))
+# for idx in range(1,101):
+#     tree.create_node('0/' + str(idx), '0/' + str(idx), parent="0", data=util.stateDataIterator(tree['0'], 2, 1))
 
 # New tree builder
 tree = util.treeGenerator((100, ), [1, 100, 100, 100], 2, 1)
-#tree = util.treeGenerator((100, 100), [1, 100, 100, 100], 2, 1)
+# tree = util.treeGenerator((100, 100), [1, 100, 100, 100], 2, 1)
 
 
-#tree.show(data_property='representation')
-#tree.show(data_property="secPrice")
-#tree.show(data_property="payOff")
+# tree.show(data_property='representation')
+# tree.show(data_property="secPrice")
+# tree.show(data_property="payOff")
 
 # Postprocessing a tree for payoffs
 util.calculatePayoffs(tree)
 
-#tree.show(data_property='representation')
-#tree.show(data_property="secPrice")
-#tree.show(data_property="payOff")
+# tree.show(data_property='representation')
+# tree.show(data_property="secPrice")
+# tree.show(data_property="payOff")
 tree.show()
 
 # Generating the GLPK input
 util.treeToLp(tree, lpFileName)
-
-
-
-
-
- 
-
-
-
 
 ##### HF2
 #tree = Tree()
