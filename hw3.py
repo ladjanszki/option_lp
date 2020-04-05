84283c3bb931e7dfca9ac2cbfc5c7343834c162c
Istvan Ladjanszki <istvan.ladjanszki@gmail.com>
2020-04-05 19:50:45 +0200

    * Some code change

--- hw3.py
+++ hw3.py
@@ -9,31 +9,28 @@ import util
 np.random.seed(0)
 
 # Prefix
-path = 'results/hw3/'
+path = 'results/hw3_uj/'
 
 if not os.path.exists(path):
     os.mkdir(path) 
-
-reportLines = []
- 
-# Maximal number of children for three level tree
-for idx, n in enumerate((5, 10, 20)):
-    res = util.measurement(path, taskId = 'hw3_' + str(idx) + '_1', childrenPerLevel = (n, n, n), initSecPrice = [1, 100, 100, 100], logNormalMean = 2, logNormalSigma = 1) 
-    res.append(n)
-    reportLines.append(res)
-
-    res = util.measurement(path, taskId = 'hw3_' + str(idx) + '_2', childrenPerLevel = (n, n, n), initSecPrice = [1, 100, 100, 100], logNormalMean = 0, logNormalSigma = 2) 
-    res.append(n)
-    reportLines.append(res)
 
 # csv report
 with open(path + 'report.csv','w') as report:
     csvWriter = csv.writer(report)
     csvWriter.writerow(['Option price', 'Wall time', 'CPU Time', 'Lognormal Mean', 'Lognormal Sigma', 'Files', 'n branch'])
-    for row in reportLines:
-        csvWriter.writerow(row)
- 
 
- 
+    # Maximal number of children for three level tree
+    #for idx, n in enumerate((5, 10, 20, 30, 50, 70, 100, 120, 135)):
+    for idx, n in enumerate((70, 100, 120, 135)):
+        res = util.measurement(path, taskId = 'hw3_' + str(idx) + '_1', childrenPerLevel = (n, n, n), initSecPrice = [1, 100, 100, 100], logNormalMean = 2, logNormalSigma = 1) 
+        res.append(n)
+        csvWriter.writerow(res)
+        report.flush()
+    
+        #res = util.measurement(path, taskId = 'hw3_' + str(idx) + '_2', childrenPerLevel = (n, n, n), initSecPrice = [1, 100, 100, 100], logNormalMean = 0, logNormalSigma = 2) 
+        #res.append(n)
+        #csvWriter.writerow(res)
+        #report.flush()
+
  
  
