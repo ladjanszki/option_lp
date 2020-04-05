48d80d25467432b84c4588dfd195223848105b10
István Ladjánszki <istvan.ladjanszki@gmail.com>
2020-04-05 19:58:16 +0200

    * some code

--- hw4.py
+++ hw4.py
@@ -15,34 +15,10 @@ if not os.path.exists(path):
     os.mkdir(path) 
 
 reportLines = []
- 
+
+branches = (5, 10, 20, 25, 30, 50)
+
 # Maximal number of children for three level tree
-for idx1, n1 in enumerate((5, 10, 20)):
-    for idx2, n2 in enumerate((5, 10, 20)):
-        for idx3, n3 in enumerate((5, 10, 20)):
-
-            res = util.measurement(path, taskId = 'hw4_' + str(idx1) + str(idx2) + str(idx3) + '_1', 
-                  childrenPerLevel = (n1, n2, n3), 
-                  initSecPrice = [1, 100, 100, 100], 
-                  logNormalMean = 2, 
-                  logNormalSigma = 1) 
-
-            res.append(n1)
-            res.append(n2)
-            res.append(n3)
-            reportLines.append(res)
-
-            res = util.measurement(path, taskId = 'hw4_' + str(idx1) + str(idx2) + str(idx3) + '_2', 
-                  childrenPerLevel = (n1, n2, n3), 
-                  initSecPrice = [1, 100, 100, 100], 
-                  logNormalMean = 0, 
-                  logNormalSigma = 2) 
-            res.append(n1)
-            res.append(n2)
-            res.append(n3)
-            reportLines.append(res)
-
-# csv report
 with open(path + 'report.csv','w') as report:
     csvWriter = csv.writer(report)
     csvWriter.writerow(['Option price', 
@@ -52,8 +28,41 @@ with open(path + 'report.csv','w') as report:
                         'Lognormal Sigma', 
                         'Files', 
                         'n1', 'n2', 'n3'])
-    for row in reportLines:
-        csvWriter.writerow(row)
+    #for row in reportLines:
+    #    csvWriter.writerow(row)
  
+    for idx1, n1 in enumerate(branches):
+        for idx2, n2 in enumerate(branches):
+            for idx3, n3 in enumerate(branches):
+    
+                res = util.measurement(path, taskId = 'hw4_' + str(idx1) + str(idx2) + str(idx3) + '_1', 
+                      childrenPerLevel = (n1, n2, n3), 
+                      initSecPrice = [1, 100, 100, 100], 
+                      logNormalMean = 2, 
+                      logNormalSigma = 1) 
+    
+                res.append(n1)
+                res.append(n2)
+                res.append(n3)
+                #reportLines.append(res)
+                csvWriter.writerow(res)
+                report.flush()
+    
+    
+    for idx1, n1 in enumerate(branches):
+        for idx2, n2 in enumerate(branches):
+            for idx3, n3 in enumerate(branches):
+                res = util.measurement(path, taskId = 'hw4_' + str(idx1) + str(idx2) + str(idx3) + '_2', 
+                      childrenPerLevel = (n1, n2, n3), 
+                      initSecPrice = [1, 100, 100, 100], 
+                      logNormalMean = 1, 
+                      logNormalSigma = 2) 
+                res.append(n1)
+                res.append(n2)
+                res.append(n3)
+                #reportLines.append(res)
+                csvWriter.writerow(res)
+                report.flush()
+
 
  
